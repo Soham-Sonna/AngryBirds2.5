7cf0b4c26a64a3cad0f0e8ce9ee57f47bff0d7f1
Soham <sonnasoham@gmail.com>
2020-06-29 20:56:45 +0530

    sketch.js

--- sketch.js
+++ sketch.js
@@ -21,8 +21,8 @@ function setup(){
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    pig1 = new Pig(810, 349);
+    log1 = new Log(809,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
